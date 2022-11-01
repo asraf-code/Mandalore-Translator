d6406345a9744d1cb2d184c9b1db3c15ddfb903c
asraf-code <64000853+asraf-code@users.noreply.github.com>
2022-11-02 01:16:55 +0530

    edited output text

--- app.js
+++ app.js
@@ -9,21 +9,18 @@ function getTranslationURL(input) {
 }
 
 function errorHandler(error) {
-    console.log("error occured", error)
     alert("something wrong with server! try again after some time")
 }
 
 function clickHandler() {
-    // outputDiv.innerText = "lhaldhla" + txtInput.value;
+    var inputText = txtInput.value;
 
-    var inputText = txtInput.value; // taking input
 
-    // calling server for processing
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; // output
+            outputDiv.innerText = translatedText;
         })
         .catch(errorHandler)
 };
